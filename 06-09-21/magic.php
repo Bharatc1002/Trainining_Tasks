3f4c211a290673ac9cdc0f7859237bf601bf4049
Bharatc1002 <bharat.c.addweb@gmail.com>
2021-09-07 18:57:21 +0530

    "adding more magic methods"

--- 06-09-21/magic.php
+++ 06-09-21/magic.php
@@ -2,7 +2,7 @@
 class Names {
     private $fName;
     private $lName;
-    public $array = array("firstName" => "Bharat", "secondName" => "Chaudhary", "names" => "write here");
+    private $array = array("firstName" => "Bharat", "secondName" => "Chaudhary", "names" => "write here");
  
     public function __construct($fname, $lname) 
     {
@@ -42,13 +42,26 @@ class Names {
     {
         return isset($this->$name);
     }
+    public static function __callStatic($name, $array){
+        echo $name . "<br>";
+        foreach($array as $arg){
+            echo $arg . "<br>";
+        }
+    }
+    public function __isset($name){
+        echo $name;
+    }
 }
  
 $name1= new Names('firstName', 'secondName'); //__construct function
 echo $name1->newName; // __get function
 echo $name1;  // __toString function
 $name1->func("Hello", "Welcome","Please sit"); //__call function
+echo $name1->names . "<br>"; //__get function
 $name1->names = "okay"; //__set function
-echo $name1->names; //__get function
+echo $name1->names . "<br>"; //__get function
+Names::world("zero", "one", "two", "three"); //__callStatic function
+isset($name1->height);
+
 
 ?>
